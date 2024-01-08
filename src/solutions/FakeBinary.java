afabe4e786d7e1ed27b73958a616e51c5eccef15
Tatiana Mamaliga <mamaliga.tatiana@gmail.com>
2024-01-08 21:38:15 +0200

    first commit

---
+++ src/solutions/FakeBinary.java
@@ -0,0 +1,15 @@
+package solutions;
+
+public class FakeBinary {
+    public static String fakeBin(String numberString) {
+        char[] charArr = numberString.toCharArray();
+        for (int i = 0; i < charArr.length; i++) {
+            if (Character.getNumericValue(charArr[i]) < 5) {
+                charArr[i] = '0';
+            } else {
+                charArr[i] = '1';
+            }
+        }
+        return new String(charArr);
+    }
+}
