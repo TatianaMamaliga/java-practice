9a819cb021f862019e74d418cae8767ba24766b3
Tatiana Mamaliga <mamaliga.tatiana@gmail.com>
2024-01-12 10:23:44 +0200

    Number of trailing zeros of factorial N solution

---
+++ src/solutions/NumberOfTrailingZerosOfNFactorial.java
@@ -0,0 +1,18 @@
+package solutions;
+
+public class NumberOfTrailingZerosOfNFactorial {
+    public int zeros(int n){
+        int num = (int)(Math.log(n)/Math.log(5));
+        int count = 0;
+        for (int i = 1; i <= num; i++){
+            count += n / Math.pow(5, i);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        NumberOfTrailingZerosOfNFactorial zerosOfNFactorial = new NumberOfTrailingZerosOfNFactorial();
+
+        System.out.println(zerosOfNFactorial.zeros(10));
+    }
+}
